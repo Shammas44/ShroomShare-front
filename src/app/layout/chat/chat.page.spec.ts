72b33e50e61d91b7f58eb2dabf6ba47ac0361904
Timothée Dione <timothee.dione@gmail.com>
2023-01-20 16:10:34 +0100

    mushrooms page ok tier

--- src/app/layout/chat/chat.page.spec.ts
+++ src/app/layout/chat/chat.page.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Storage } from '@ionic/storage';
 
 import { ChatPage } from './chat.page';
